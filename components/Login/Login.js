778990f1a8c1a8026614ea4a1adeca777c2ecadc
prem-1981 <glitzgalatta@gmail.com>
2020-05-19 20:25:03 +0530

    1 file modified

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -53,7 +53,6 @@ render() {
   
     return(
         <div className="container">
-        <img src ="../images/bg.jpg" />
         <div className="row">
           <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
             <div className="card card-signin my-5">
