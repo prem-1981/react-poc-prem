77780237433b370625d798c6afd286cbd5cf4d19
prem-1981 <glitzgalatta@gmail.com>
2020-05-22 19:42:25 +0530

    2 files modified

--- App.js
+++ App.js
@@ -9,15 +9,26 @@ import {
 import Login  from './components/Login/Login';
 import LandingPage from './components/LandingPage/LandingPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user:false
+    }
+  }
+  handleLogin = (user) => {
+    this.setState({
+      user:true
+    })
+   console.log("...." + this.state.user)
+  }
   render(){
     return (
       <Router>
-      <div className="container">
+      <div className="container-fluid">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
-  <Link to="#" className="navbar-brand" > V-MART  </Link>
+  <Link to="#" className="navbar-brand" > V-MART</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -27,6 +38,7 @@ class App extends React.Component {
       {/* <li className="nav-item active">
         <Link to="/landing" className="nav-link" > Home </Link> 
       </li> */}
+      <li className="nav-item"> {this.state.user.toString()} </li>
     </ul>
     </div>
     </nav>
@@ -34,17 +46,16 @@ class App extends React.Component {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path={"/"}
+          <Route exact path={"/"} 
           render={props => (
-            <Login  {...props} />
+            <Login  {...props} user={this.state.user.toString()} handleLogin = {this.handleLogin}/>
           )}>
           </Route>
           <Route
               exact
               path={"/landing"}
               render={props => (
-                <LandingPage
-                  {...props}
+                <LandingPage user={this.state.user.toString()}    {...props} 
                 />
               )}
             />
--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,14 +1,13 @@
 /* eslint-disable */ 
 import React from 'react';
 import render from 'react-dom';
-
+import './login.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,Redirect
 } from "react-router-dom";
-import './login.css';
 
 class Header extends React.Component{
 constructor(props){
@@ -20,15 +19,16 @@ constructor(props){
         error: '',
         errorMessages: ''
     }
-    this.handleSubmit = this.handleSubmit.bind(this);
+   // this.handleSubmit = this.handleSubmit.bind(this);
 }
 
-handleInputChange(e) {
+
+handleInputChange = (e)=> {
   this.setState({ [e.target.name]: e.target.value });
   this.setState({ errorMessages: '' });
 }
 
-handleSubmit(e){
+handleSubmit = (e)=> {
   e.preventDefault();
   this.setState({ submitted: true });
   const {username,password} = this.state;
@@ -37,7 +37,12 @@ handleSubmit(e){
     return;
   }
   if(username==='shop' && password==='shop'){
-    this.props.history.push("/landing");
+    console.log("======" + this.props.user)
+    this.props.handleLogin();
+      setTimeout(() => {
+        this.props.history.push("/landing");
+      }, 3000);
+   
   }else{
     
     this.setState({ errorMessages: "Please Enter Correct Usernamer and Password" });
@@ -50,7 +55,9 @@ render() {
   
   const { username, password, submitted } = this.state;
 
-  
+//   return(<div class="spinner-border" role="status">
+//   <span class="sr-only">Loading...</span>
+// </div>) 
     return(
         <div className="container">
         <div className="row">
@@ -58,8 +65,10 @@ render() {
             <div className="card card-signin my-5">
               <div className="card-body">
                 <h5 className="card-title text-center">Sign In</h5>
+                <h4> ===> LoggedIn Status {this.props.user} </h4>
                <p className="has-error"> {this.state.errorMessages} </p>
-                <form className="form-signin" name="form" onSubmit={this.handleSubmit}>
+              
+                <form className="form-signin" name="form">
                   <div className={"form-label-group" + (submitted && !username ? ' has-error' : '')}>
 
                     <input type="text" id="inputEmail" 
@@ -97,7 +106,7 @@ render() {
 
                   </div>
 
-                  <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">
+                  <button onClick={this.handleSubmit} className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">
                      Sign in
                  </button>
                 </form>
